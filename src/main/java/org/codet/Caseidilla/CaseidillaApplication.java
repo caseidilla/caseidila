2d0d0dc4f24e6ec31621d7860754f926975ef3f5
Александр Тарасов <tarasovae97@mail.ru>
2018-10-02 17:50:06 +0300

    Initial commit

---
+++ src/main/java/org/codet/Caseidilla/CaseidillaApplication.java
@@ -0,0 +1,12 @@
+package org.codet.Caseidilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseidillaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseidillaApplication.class, args);
+    }
+}
